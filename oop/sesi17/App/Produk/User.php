b943522588e331bf44e82e54caddc54797fc2e37
joofrederik <121954413+joofrederik@users.noreply.github.com>
2024-10-28 21:03:58 +0700

    tugas oop

---
+++ oop/sesi17/App/Produk/User.php
@@ -0,0 +1,12 @@
+<?php
+// namespace vendor/app\namespace\subnamespace
+namespace App\Produk;
+
+class User
+{
+
+    public function __construct()
+    {
+        echo "Ini adalah class " . __CLASS__ . "";
+    }
+}
