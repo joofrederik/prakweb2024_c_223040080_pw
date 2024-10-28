277d80741bc8ad47b836b8f3402a50f1fdb3f093
joofrederik <121954413+joofrederik@users.noreply.github.com>
2024-10-28 20:49:47 +0700

    tugas oop

---
+++ oop/sesi16/App/Produk/sesi17/App/Produk/InfoProduk.php
@@ -0,0 +1,12 @@
+<?php
+
+// nama kelas = nama file
+
+// interfaace
+interface InfoProduk
+{
+    // karena interface ini harus public dan tidak ada property
+    // dan tulisannya implements
+    // hanya interface tanpa isi
+    public function getInfoProduk();
+}
